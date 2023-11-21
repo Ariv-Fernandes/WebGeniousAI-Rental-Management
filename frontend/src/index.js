af4a893f371a51c6e2fe900168d6cc7f7a20629b
KeshavD01 <ksdhanuka_b22@it.vjti.ac.in>
2023-11-21 11:36:42 +0530

    review added

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from '/Users/keshavdhanuka01/Desktop/WebGeniousAI-Rental-Management/frontend/src/Reviews/App.js';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+

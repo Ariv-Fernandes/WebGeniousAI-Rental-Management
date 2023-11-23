13195da083133cb9211a780e07f7b004b8a9edf2
KeshavD01 <ksdhanuka_b22@it.vjti.ac.in>
2023-11-23 22:02:24 +0530

    reviews changed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from '/Users/keshavdhanuka01/Desktop/WebGeniousAI-Rental-Management/frontend/src/Reviews/App.js';
 
 

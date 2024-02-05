fc8ff8c46a82d5c422916c2ae7630cba5661d081
Denish004 <denishsuhagiya39@gmail.com>
2024-02-05 13:53:58 +0530

    solve commit

--- src/Homepage/Frontend/Components/Name.js
+++
@@ -1,9 +0,0 @@
-function Name(){
-return(
-    <div class="my-20 h-60 ">
-        <h1 class="font-bold px-10 pt-10 pb-8 mb-10 mx-10 text-center  text-6xl">Lorem ipsum dolor sit amet.</h1>
-        <p class="text-center my-10">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum soluta dolor quam. Vero, facilis delectus enim ducimus maxime quo ut.</p>
-    </div>
-)
-}
-export default Name
